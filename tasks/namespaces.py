96fbcb48fa54395dc562b734a90e97f8aa45a6bf
spelak-salesforce <spelak@salesforce.com>
2019-10-30 11:21:12 -0400

    namespaces: caches installed_package_versions on org_config.config; updated is_namespaced_used

--- tasks/namespaces.py
+++ tasks/namespaces.py
@@ -6,17 +6,17 @@ from cumulusci.tasks.salesforce import GetInstalledPackages
 class NamespaceInfo(TableLogger):
     installed_package_versions = None
 
-    log_installed_packages = False
-
     def get_installed_package_versions(self):
-        if self.installed_package_versions:
-            return self.installed_package_versions
+        if self.org_config.config.get("installed_package_versions"):
+            return self.org_config.config.get("installed_package_versions")
         
         log_installed_packages = process_bool_arg(self.options.get("log_installed_packages"))
 
         self.log_title("Getting installed packages:")
 
-        self.installed_package_versions = {}
+        installed_package_versions = {}
+
+        self.org_config.config["installed_package_versions"] = installed_package_versions
 
         if log_installed_packages:
             rows = [
@@ -31,7 +31,7 @@ class NamespaceInfo(TableLogger):
             TaskConfig({}), 
             self.org_config,
         )().items():
-            self.installed_package_versions[package[0]] = package[1]
+            installed_package_versions[package[0]] = package[1]
             if log_installed_packages:
                 rows.append([
                     package[0],
@@ -41,7 +41,7 @@ class NamespaceInfo(TableLogger):
         if log_installed_packages:
             self.log_table(rows)
 
-        return self.installed_package_versions
+        return installed_package_versions
 
     def get_installed_package_namespaces(self):
         return self.get_installed_package_versions().keys()
@@ -49,17 +49,17 @@ class NamespaceInfo(TableLogger):
     def get_project_namespace(self):
         return self.project_config.project__package__namespace
 
-    def is_project_namespaced(self):
+    def is_org_namespaced(self):
         return self.org_config and process_bool_arg(self.org_config.config.get("namespaced"))
 
     def get_local_project_namespace(self):
-        return "" if not self.is_project_namespaced() else self.get_project_namespace()
+        return "" if not self.is_org_namespaced() else self.get_project_namespace()
 
     def is_package_installed(self, namespace):
         return namespace in self.get_installed_package_namespaces()
 
-    def is_project_namespace(self, namespace):
-        return namespace in self.get_installed_package_namespaces()
-
     def is_namespace_used(self, namespace):
-        return self.is_project_namespace(namespace) or self.is_package_installed(namespace)
+        if self.get_project_namespace() == namespace:
+            return self.is_org_namespaced()
+        else:
+            return self.is_package_installed(namespace)

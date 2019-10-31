300a9599c34d70cf19810cfd5c9009ac0c16f9ef
Beth Breisnes <bbreisnes@salesforce.com>
2019-10-31 11:05:30 -0700

    revert lwc changes in this branch

--- force-app/main/default/lwc/bulkServiceDeliveryEntry/bulkServiceDeliveryEntry.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track } from "lwc";
-
-export default class BulkServiceDeliveryEntry extends LightningElement {
-    @track serviceDeliveries = [{ Id: null }];
-
-    createNewServiceDelivery() {
-        this.serviceDeliveries.push({ Id: null });
-    }
-}

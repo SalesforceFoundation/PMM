7895189876b694ab8fdac02966f31560bce0ac21
gaganpsandhu <gsandhu@salesforce.com>
2019-11-13 10:31:20 -0800

    adding test for creating a new program

---
+++ robot/pmdm/resources/pmdm_locators.py
@@ -0,0 +1,7 @@
+pmdm_lex_locators = {
+    "app_launcher": {
+        "waffle": "//div[contains(@class, 'slds-icon-waffle')]",
+        "select-option": "//li[contains(@class,'oneAppLauncherItem')]//a[@title='{}']",
+    },
+"text": "//*[contains(text(), '{}')]",
+}

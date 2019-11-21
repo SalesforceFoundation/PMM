c8cc57652c256f82a9e3f24ca82f4f066c9b2b37
Bharathi Muniswamy <bmuniswamy@salesforce.com>
2019-11-21 13:48:18 -0500

    Removed hard coded strings and added a label.  Removed the event handling from ServiceDeliveryModal html file.

--- force-app/main/default/lwc/serviceDeliveryModal/serviceDeliveryModal.js
+++ force-app/main/default/lwc/serviceDeliveryModal/serviceDeliveryModal.js
@@ -1,12 +1,17 @@
 import { LightningElement, wire } from "lwc";
 import { registerListener } from "c/pubsub";
 import { CurrentPageReference } from "lightning/navigation";
+import serviceDeliveries from "@salesforce/label/c.Service_Deliveries";
 
 export default class serviceDeliveryModal extends LightningElement {
     @wire(CurrentPageReference) pageRef;
 
+    labels = {
+        serviceDeliveries,
+    };
+
     connectedCallback() {
-        registerListener("openpmdmbulkservicedelivery", this.handleOpenModal, this);
+        registerListener("OpenPmdmBulkServiceDelivery", this.handleOpenModal, this);
     }
 
     handleOpenModal() {

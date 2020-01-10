d38fd785c857586432fd441f1c4c65cffd01b793
Will <wwatkins@salesforce.com>
2020-01-10 16:37:38 -0600

    update to icon

--- force-app/main/default/lwc/serviceDeliveryRowIcon/serviceDeliveryRowIcon.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from "lwc";
-export default class ServiceDeliveryRowIcon extends LightningElement {
-    @api iconName;
-    @api className;
-    @api alternativeText;
-    @api size;
-    @api variant;
-    @api helpText;
-}

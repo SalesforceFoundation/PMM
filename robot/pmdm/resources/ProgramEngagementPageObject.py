6d739985d60e0f0ece721fe629aed04bbf13640a
gaganpsandhu <gsandhu@salesforce.com>
2020-01-28 11:14:36 -0800

    Adding new test to check Auto name override for PE

--- robot/pmdm/resources/ProgramEngagementPageObject.py
+++ robot/pmdm/resources/ProgramEngagementPageObject.py
@@ -54,6 +54,10 @@ class NewProgramEngagementPage(BasePMDMPage, BasePage):
             else:
                 assert False, "Key provided by name '{}' does not exist".format(key)
 
+    def select_auto_name_override_checkbox(self):
+        locator = pmdm_lex_locators["checkbox"].format('Auto-Name Override')
+        self.selenium.get_webelement(locator).click()
+
 
 @pageobject("Details", "ProgramEngagement__c")
 class ProgramDetailPage(BasePMDMPage, DetailPage):

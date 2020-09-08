e35c601112dba1f72a97045b3ed1147f92570e40
David Reed <d.reed@salesforce.com>
2020-09-08 16:25:00 -0600

    Ignore Jest tests in .cci

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 module.exports = {
     ...jestConfig,
-    testPathIgnorePatterns: ["force-app/main/default/lwc/__tests__/"],
+    testPathIgnorePatterns: ["force-app/main/default/lwc/__tests__/", ".cci"],
     reporters: ["default", "jest-junit"],
     setupFilesAfterEnv: ["./jest.setup.js"]
 };

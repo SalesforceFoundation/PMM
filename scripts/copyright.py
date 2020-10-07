b5df11d2d79e66c5896c746c4f9c5805c6217f25
Beth Breisnes <bbreisnes@salesforce.com>
2020-10-07 16:40:03 -0700

    add html, cmp, and css support for copyrighting

--- scripts/copyright.py
+++ scripts/copyright.py
@@ -11,24 +11,8 @@ def get_all_paths(path):
     return glob.glob(path, recursive=True)
 
 
-def main():
-    js_paths = get_all_paths("force-app/**/*.js")
-    apex_paths = get_all_paths("force-app/**/*.cls")
-
-    paths = js_paths + apex_paths
-
-    copyright = """/*
- *
- *  * Copyright (c) 2020, salesforce.com, inc.
- *  * All rights reserved.
- *  * SPDX-License-Identifier: BSD-3-Clause
- *  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- *
- */
-
-"""
+def apply_copyright(paths, copyright):
     total = 0
-
     for path in paths:
         text = Path(path).read_text()
         top_lines = text.splitlines()[:9]
@@ -38,10 +22,47 @@ def main():
                 Path(path).write_text(text)
                 total += 1
                 break
+    return total
+
+
+def main():
+    js_paths = get_all_paths("force-app/**/*.js")
+    apex_paths = get_all_paths("force-app/**/*.cls")
+    css_paths = get_all_paths("force-app/**/*.css")
+    star_paths = js_paths + apex_paths + css_paths
+
+    html_paths = get_all_paths("force-app/**/*.html")
+    cmp_paths = get_all_paths("force-app/**/*.cmp")
+    pointy_paths = html_paths + cmp_paths
+
+    star_copyright = """/*
+ *
+ *  * Copyright (c) 2020, salesforce.com, inc.
+ *  * All rights reserved.
+ *  * SPDX-License-Identifier: BSD-3-Clause
+ *  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
+"""
+    pointy_copyright = """<!--
+  - /*
+  -  * Copyright (c) 2020, salesforce.com, inc.
+  -  * All rights reserved.
+  -  * SPDX-License-Identifier: BSD-3-Clause
+  -  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+  -  */
+  -->
+
+"""
+
+    total = apply_copyright(pointy_paths, pointy_copyright) + apply_copyright(
+        star_paths, star_copyright
+    )
 
     if total == 0:
         print(
-            "All .cls and .js files already have copyright statements! \U0001F389 \U000000A9 \U0001F389 \U000000A9 \U0001F389"
+            "All .cls, .js, .html, .cmp, and .css files already have copyright statements! \U0001F389 \U000000A9 \U0001F389 \U000000A9 \U0001F389"
         )
     else:
         print(f"Added copyright statement to {total} files")

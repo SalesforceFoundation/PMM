105db59be6ac1df3619bfc98e87651145111ecef
Jennifer Bennett <jennifer.bennett@salesforce.com>
2021-06-14 10:04:33 -0500

    Display spinner when loading data from apex.

--- force-app/main/default/lwc/participantSelector/participantSelector.js
+++ force-app/main/default/lwc/participantSelector/participantSelector.js
@@ -27,6 +27,7 @@ import filterByRecord from "@salesforce/label/c.Filter_By_Record";
 import noContactsSelected from "@salesforce/label/c.No_Service_Participants_Created_Warning";
 import add from "@salesforce/label/c.Add";
 import addAll from "@salesforce/label/c.Add_All";
+import loading from "@salesforce/label/c.Loading";
 export default class ParticipantSelector extends LightningElement {
     @api serviceId;
     @api serviceSchedule;
@@ -57,6 +58,7 @@ export default class ParticipantSelector extends LightningElement {
     offset = this.offsetRows;
 
     labels = {
+        loading,
         selectContacts,
         selectedContacts,
         capacityWarning,

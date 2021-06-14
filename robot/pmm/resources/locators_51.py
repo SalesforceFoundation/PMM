26784c1910b8d932023eac2785c83a6a9e6a75f6
Monica Ramachandran <48073123+rmonica08@users.noreply.github.com>
2021-06-14 11:31:02 -0400

    updated listview locator

--- robot/pmm/resources/locators_51.py
+++ robot/pmm/resources/locators_51.py
@@ -59,7 +59,7 @@ pmm_lex_locators = {
         "related_list": "//a[contains(@class,'slds-card')]/span[contains(@class,'slds-truncate') and @title='{}']",
     },
     "quick_actions": "//button[@class='slds-button slds-button_neutral' and text()='{}']",
-    "list_view": "//a[contains(@class,'slds-button') and @title='Select List View']",
+    "list_view": "//button[contains(@class,'slds-button') and @title='Select List View']",
     "listview_options": "//li[contains(@class,'slds-dropdown__item')]//a//span[text()='{}']",
     "listview_link": "//a[contains(@class,'slds-truncate') and text()='{}']",
     "toast_link": "//span[contains(@class,'toastMessage')]/a[@class='forceActionLink']/div[contains(@title,'{}')]",

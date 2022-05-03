f6ca0b6a2b5002f46bd38941b9a2315a29825926
Patrick Tewson <ptewson@salesforce.com>
2022-05-02 17:06:20 -0700

    Load existing engagements

--- force-app/main/default/lwc/serviceScheduleAddSessionsWrapper/serviceScheduleAddSessionsWrapper.js
+++ force-app/main/default/lwc/serviceScheduleAddSessionsWrapper/serviceScheduleAddSessionsWrapper.js
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright (c) 2021, salesforce.com, inc.
+ *  * Copyright (c) 2022, salesforce.com, inc.
  *  * All rights reserved.
  *  * SPDX-License-Identifier: BSD-3-Clause
  *  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -10,15 +10,9 @@
 import { LightningElement, api } from "lwc";
 import newServiceSchedule from "@salesforce/label/c.New_Service_Schedule";
 
-export default class ServiceScheduleCreatorWrapper extends LightningElement {
-    @api recordTypeId;
+export default class ServiceScheduleAddSessionsWrapper extends LightningElement {
     @api recordId;
     @api isCommunity;
-    @api serviceId;
 
     labels = { newServiceSchedule };
-
-    handleClose() {
-        this.template.querySelector("c-service-schedule-creator").handleClose();
-    }
 }

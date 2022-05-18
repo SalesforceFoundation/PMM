f28235e7ba888d8784cb283bf361bc1dab19113e
Patrick Tewson <ptewson@salesforce.com>
2022-05-18 09:56:13 -0700

    increase width of add more sessions modal

--- force-app/main/default/lwc/serviceScheduleAddSessionsQuickAction/serviceScheduleAddSessionsQuickAction.js
+++ force-app/main/default/lwc/serviceScheduleAddSessionsQuickAction/serviceScheduleAddSessionsQuickAction.js
@@ -9,10 +9,16 @@
 
 import { LightningElement, api } from "lwc";
 import addServiceSessions from "@salesforce/label/c.Add_More_Sessions";
+import pmmFolder from "@salesforce/resourceUrl/pmm";
+import { loadStyle } from "lightning/platformResourceLoader";
 
 export default class ServiceScheduleAddSessionsQuickAction extends LightningElement {
     @api recordId;
     @api isCommunity;
 
     labels = { addServiceSessions };
+
+    connectedCallback() {
+        Promise.all([loadStyle(this, pmmFolder + "/quickActionModal.css")]);
+    }
 }

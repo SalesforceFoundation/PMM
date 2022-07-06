5d0c9039d889a050192660a73d0106f7878b832d
Patrick Tewson <ptewson@salesforce.com>
2022-07-06 11:55:28 -0700

    Add grab focus bevavior to section and apply in BSDT + participant selector

--- force-app/main/default/lwc/section/section.js
+++ force-app/main/default/lwc/section/section.js
@@ -11,4 +11,24 @@ import { LightningElement, api } from "lwc";
 
 export default class Section extends LightningElement {
     @api title;
+    @api grabFocus = false;
+
+    connectedCallback() {
+        if (this.grabFocus) {
+            this.handleFocus();
+        }
+    }
+
+    handleFocus() {
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        setTimeout(
+            function() {
+                let firstInput = this.template.querySelector("h3");
+                if (firstInput) {
+                    firstInput.focus();
+                }
+            }.bind(this),
+            50
+        );
+    }
 }
